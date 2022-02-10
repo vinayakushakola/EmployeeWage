c090b986ddf7bb90ae28cbd84e0f3ac4b8c9c92d
vinayak ushakola <vinayak.ushakola@gmail.com>
2022-02-10 22:16:19 +0530

    Added Employee Attendance Functionality

---
+++ EmployeeWageComputation/EmpWageComputation.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EmployeeWageComputation
+{
+    public class EmpWageComputation
+    {
+        const int IS_PRESENT = 1;
+        Random generateNum = new Random();
+
+        public void IsEmployeePresent()
+        {
+            int check = generateNum.Next(0, 2);
+            if (check == IS_PRESENT)
+            {
+                Console.WriteLine("Employee is Present");
+            }
+            else
+            {
+                Console.WriteLine("Employee is Absent");
+            }
+        }
+    }
+}
--- EmployeeWageComputation/Program.cs
+++ EmployeeWageComputation/Program.cs
@@ -7,6 +7,9 @@ namespace EmployeeWageComputation
         static void Main(string[] args)
         {
             Console.WriteLine("========== Welcome To Employee Wage Computation ===========");
+
+            EmpWageComputation emp = new EmpWageComputation();
+            emp.IsEmployeePresent();
         }
     }
 }
