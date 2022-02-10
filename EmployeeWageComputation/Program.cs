5de254308323704d553eb73d722ce02ec078861c
vinayak ushakola <vinayak.ushakola@gmail.com>
2022-02-10 22:05:52 +0530

    Added Welcome Message

---
+++ EmployeeWageComputation/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace EmployeeWageComputation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("========== Welcome To Employee Wage Computation ===========");
+        }
+    }
+}
